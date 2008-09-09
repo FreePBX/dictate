ee19382cc46d3ef73149068c37503db90ae70371
Micke Carlsson <mickecamino@gmail.com>
2008-09-09 11:19:52 +0000

    Added swedish translation and enclosed some text strings for dictate module

--- install.php
+++ install.php
@@ -1,15 +1,17 @@
 <?php
 
 // Register Feature Code - Perform Dictation
+$performdictate = _("Perform dictation");
 $fcc = new featurecode('dictate', 'dodictate');
-$fcc->setDescription('Perform dictation');
+$fcc->setDescription($performdictate);
 $fcc->setDefault('*34');
 $fcc->update();
 unset($fcc);
 
 // Email dictation to user
+$emaildictation = _("Email completed dictation");
 $fcc = new featurecode('dictate', 'senddictate');
-$fcc->setDescription('Email completed dictation');
+$fcc->setDescription($emaildictation);
 $fcc->setDefault('*35');
 $fcc->update();
 unset($fcc);
