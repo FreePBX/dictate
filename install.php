42eb273010ec1d2f725c290f5ed8acad8523afca
Micke Carlsson <mickecamino@gmail.com>
2008-09-29 20:49:49 +0000

    Reverting previous changes, add strings for localization for dictation

--- install.php
+++ install.php
@@ -1,17 +1,21 @@
 <?php
+//for translation only
+if (false) {
+_("Dictation");
+_("Perform dictation");
+_("Email completed dictation");
+}
 
 // Register Feature Code - Perform Dictation
-$performdictate = _("Perform dictation");
 $fcc = new featurecode('dictate', 'dodictate');
-$fcc->setDescription($performdictate);
+$fcc->setDescription('Perform dictation');
 $fcc->setDefault('*34');
 $fcc->update();
 unset($fcc);
 
 // Email dictation to user
-$emaildictation = _("Email completed dictation");
 $fcc = new featurecode('dictate', 'senddictate');
-$fcc->setDescription($emaildictation);
+$fcc->setDescription('Email completed dictation');
 $fcc->setDefault('*35');
 $fcc->update();
 unset($fcc);
